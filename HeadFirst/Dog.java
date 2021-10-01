3652fa3e6efc46b8ab1e49da0aa2c4bdbb781035
Andrea Chavarri <achava7@yahoo.com>
2021-10-01 11:17:47 -0400

    dog class

--- HeadFirst/Dog.java
+++ HeadFirst/Dog.java
@@ -2,19 +2,23 @@ class Dog {
     int size;
     String name;
 
-    void bark () {
-        if (size > 60) {
+    void bark (int numOfBarks) {
+        /*if (size > 60) {
             System.out.println("Wooof! Wooof!");
         } else if (size > 14) {
             System.out.println("Ruff! Ruff!");
         } else {
             System.out.println("Yip! Yip!");
+        }*/
+        while (numOfBarks > 0){
+            System.out.println("ruff");
+            numOfBarks = numOfBarks - 1;
         }
     }
 }
 class DogTestDrive {
     public static void main (String[] args) {
-        Dog one = new Dog();
+        /* Dog one = new Dog();
         one.size = 70;
         Dog two = new Dog();
         two.size = 8;
@@ -24,5 +28,8 @@ class DogTestDrive {
         one.bark();
         two.bark();
         three.bark();
+        */
+        Dog one = new Dog();
+        one.bark(3);
     }
 }
