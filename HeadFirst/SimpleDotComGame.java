81d7e55b87a849c8069e41959fa6d3faaa6affba
Andrea Chavarri <achava7@yahoo.com>
2021-10-06 22:29:04 -0400

    start battleship-like game

---
+++ HeadFirst/SimpleDotComGame.java
@@ -0,0 +1,14 @@
+class SimpleDotCom {
+    int locationCells;
+    int numOfHits;
+
+    String checkYourself (String guess) {
+        return 
+    }
+}
+
+class SimpleDotComGame {
+    public static void main (String args[]) {
+
+    }
+}
