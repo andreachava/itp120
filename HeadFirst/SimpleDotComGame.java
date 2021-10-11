9cc4d106fe3ccca50bad544a8fbd6eced3a40d33
Andrea Chavarri <achava7@yahoo.com>
2021-10-11 15:49:27 -0400

    add DotCom methods

--- HeadFirst/SimpleDotComGame.java
+++ HeadFirst/SimpleDotComGame.java
@@ -1,14 +1,46 @@
+import java.util.Scanner; 
+
 class SimpleDotCom {
-    int locationCells;
+    int [] locationCells;
     int numOfHits;
 
-    String checkYourself (String guess) {
-        return 
+    String checkYourself (String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "miss";
+        for (int cell : locationCells){
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result;
+    }
+    void setLocationCells(int[] cellLocations) {
+
     }
 }
 
 class SimpleDotComGame {
-    public static void main (String args[]) {
-
+    public static void main(String [] args) {
+        SimpleDotCom game = new SimpleDotCom();
+        GameHelper helper = new GameHelper();
+        int numOfGuesses = 0;
+        int randomNum = (int)(Math.random()*5);
+        int [] locations = {randomNum, randomNum+1, randomNum+2};
+        game.setLocationCells(locations);
+        boolean isAlive = True;
+        while (isAlive == True) {
+            String guess = helper.getUserInput("Enter a number from 0 to 7: ");
+            String result = game.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("kill")){
+                
+            }
+        }
     }
 }
---
+++ HeadFirst/SimpleDotComTest.java
@@ -0,0 +1,8 @@
+class SimpleDotComTest {
+    public static void main (String args[]) {
+        SimpleDotCom test = new SimpleDotCom();
+        int[] locations = {2, 3, 4};
+        test.setLocationCells(locations);
+        String stringGuess = "2";
+        test.checkYourself(stringGuess);
+}
