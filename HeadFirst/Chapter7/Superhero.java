4a8125fabb41e85a4e366ef0dbfd982a5ec96040
Andrea Chavarri <achava7@yahoo.com>
2021-10-21 12:21:54 -0400

    add note in Superhero

--- HeadFirst/Chapter7/Superhero.java
+++ HeadFirst/Chapter7/Superhero.java
@@ -9,7 +9,7 @@ class Superhero {
 }
 
 
-class main {
+class main /* don't use this name for future ref (use TestDrive or smth) */ {
     public static void main(String [] args) {
         Superhero b = new Superhero();
         FriedEggMan a = new FriedEggMan();
@@ -19,4 +19,4 @@ class main {
         c.useSpecialPower();
     }
 }
-// :/ gonna have to come bak to this later
+// :/ gonna have to come back to this later
