ab4924d88d48eca6ad2467bba2cdc5032a802694
achavarri <>
2021-11-30 16:35:35 +0000

    output in txt file

---
+++ HeadFirst/Chapter10/Onecode.java
@@ -0,0 +1,7 @@
+public class Onecode {
+  public static void main (String[] args) {
+    int a = 42;
+    String ex = String.format("If you remember Jeff's theory on Hitchhiker's Guide to the Galaxy, you'll know that %d is the decimal code for %c", 42, 42);
+    System.out.println(ex);
+  }
+}
